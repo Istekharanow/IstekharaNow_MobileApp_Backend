6b5299f49c5f3493436b21515f6e167db1845ab8
naveedBitstek <naveed.syed@bitstek.io>
2026-03-16 15:18:17 +0530

    del bug

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -15,6 +15,7 @@ router.get('/auth/decode-token', userController.decodeCognitoCode);
 // social auth routes for mobile
 router.post('/auth/social/mobile', userController.mobileSocialLogin);
 // delete user account 
-router.delete('/delete-user', authenticate, userController.deleteUser);
+// router.delete('/delete-user', authenticate, userController.deleteUser);
+router.post('/delete-user', authenticate, userController.deleteUser);
 
 module.exports = router;
